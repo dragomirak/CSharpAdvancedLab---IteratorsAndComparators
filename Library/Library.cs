8d262b7bf31656ee532433c5dabbede425cf708b
Dragomira <132193889+dragomirak@users.noreply.github.com>
2024-02-07 18:33:51 +0200

    Add project files.

---
+++ Library/Library.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+
+namespace IteratorsAndComparators;
+
+public class Library : IEnumerable<Book>
+{
+    private readonly SortedSet<Book> books;
+    public Library(params Book[] books)
+    {
+        this.books = new SortedSet<Book>(books, new BookComparator());
+    }
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        return new LibraryIterator(books);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    private class LibraryIterator : IEnumerator<Book>
+    {
+        private readonly List<Book> books;
+        private int currentIndex;
+
+        public LibraryIterator(IEnumerable<Book> books)
+        {
+            Reset();
+            this.books = new List<Book>(books);
+        }
+        public Book Current => books[currentIndex];
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            
+        }
+
+        public bool MoveNext()
+        {
+            return ++currentIndex < books.Count;
+        }
+
+        public void Reset()
+        {
+            currentIndex = -1;
+        }
+    }
+}
